d3fe984a280bd159936c5f9d2d18e5d3527ee4f1
haniifac <haniifcandra@gmail.com>
2023-01-18 19:19:10 +0700

    initial commit

---
+++ app/src/main/java/com/magang_sti/e_tix_dummy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.magang_sti.e_tix_dummy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
