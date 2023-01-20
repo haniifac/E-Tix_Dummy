7729adacca853c1baa3a1900b5e62d4b931d3f84
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-01-20 10:02:11 +0700

    add dialog when logout

--- app/src/main/java/com/magang_sti/e_tix_dummy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/magang_sti/e_tix_dummy/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
-package com.magang_sti.e_tix_dummy.ui
+package com.magang_sti.e_tix_dummy.ui.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -30,7 +31,24 @@ class HomeFragment : Fragment() {
 
     private fun setListener() {
         binding.btnLogout.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+            showDialogLogout()
         }
+    }
+
+    private fun gotoLogin() {
+        findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+    }
+
+    private fun showDialogLogout(){
+        val dialog = AlertDialog.Builder(requireContext())
+        dialog.setTitle("Logout")
+        dialog.setMessage("Are you sure want to logout?")
+        dialog.setPositiveButton("Yes"){dialog, which ->
+            gotoLogin()
+        }
+        dialog.setNegativeButton("No"){dialog, which ->
+            dialog.dismiss()
+        }
+        dialog.create().show()
     }
 }
